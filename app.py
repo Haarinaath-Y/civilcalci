1d00701e28f1130582ba7b20d32545dfeea9114e
Nameless Code <harishy.bio@gmail.com>
2024-10-30 18:33:38 +0530

    iteration

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ st.set_page_config(page_title="Civil Material Calculator", page_icon=":material/
 
 st.title("Civil Material Calculator")
 
+
 # Function to calculate the volume
-def calculate_volume(length, breadth, thickness, width):
-    return length * breadth * thickness * width
+def calculate_volume(ln, br, thick, wid):
+    return ln * br * thick * wid
+
 
 # Update default values to floats
 default_values = {
@@ -21,9 +23,11 @@ default_values = {
 if 'add_items' not in st.session_state:
     st.session_state.add_items = [default_values.copy()]  # Use copy to avoid reference issues
 
+
 # Add a new empty add items row
 def add_item_row():
     st.session_state.add_items.append(default_values.copy())  # Use copy to avoid reference issues
+
 
 # Function to remove an item row by index with specific handling for index 0
 def remove_item_row(index):
@@ -34,12 +38,14 @@ def remove_item_row(index):
     elif index < len(st.session_state.add_items):
         del st.session_state.add_items[index]
 
+
 total_sum = 0
 
 # Display current add items
 for i in range(len(st.session_state.add_items)):
     items = st.session_state.add_items[i]  # Get the item at the current index
     item_name = st.text_input(f"Enter item {i + 1}", value=items['item_name'], key=f"item_name_{i}")
+    st.session_state.add_items[i]['item_name'] = item_name
 
     col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 0.5, 0.15, 0.15])
 
@@ -50,7 +56,8 @@ for i in range(len(st.session_state.add_items)):
         breadth = st.number_input("Enter the breadth", value=float(items['breadth']), min_value=0.0, key=f'breadth_{i}')
 
     with col3:
-        thickness = st.number_input("Enter the thickness", value=float(items['thickness']), min_value=0.0, key=f'thickness_{i}')
+        thickness = st.number_input("Enter the thickness", value=float(items['thickness']), min_value=0.0,
+                                    key=f'thickness_{i}')
 
     with col4:
         width = st.number_input("Enter the width", value=float(items['width']), min_value=0.0, key=f'width_{i}')
@@ -73,4 +80,7 @@ for i in range(len(st.session_state.add_items)):
             remove_item_row(i)
             st.rerun()  # Rerun to refresh the UI after deletion
 
+
+for item in st.session_state.add_items:
+    st.write(item['item_name'])
 st.success(f"Total Volume of all items: **{total_sum}**")
