01ea4b96a98098ace44a4a89924e4ddde6af994e
Nameless Code <harishy.bio@gmail.com>
2024-10-30 13:09:52 +0530

    column addition

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ import streamlit as st
 
 st.set_page_config(page_title="Civil Material Calculator", page_icon=":material/calculate:", layout="wide")
 
+
+def func():
+    a = length * breadth * thickness * width
+    return a
+
+
 st.title("Civil Material Calculator")
 
 item_name = st.text_input("Enter item name")
 
-col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
 with col1:
     length = st.number_input("Enter the length", value=1)
 with col2:
@@ -15,11 +21,6 @@ with col3:
     thickness = st.number_input("Enter the thickness", value=1)
 with col4:
     width = st.number_input("Enter the width", value=1)
+with col5:
+    st.info(func())
 
-
-def func():
-    a = length * breadth * thickness * width
-    return a
-
-
-st.write(func())
