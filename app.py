e22ef3682537f37e497d74f9fe3fb46fe5ef991e
Nameless Code <harishy.bio@gmail.com>
2024-10-30 18:19:19 +0530

    iteration

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ st.set_page_config(page_title="Civil Material Calculator", page_icon=":material/
 
 st.title("Civil Material Calculator")
 
-
 # Function to calculate the volume
 def calculate_volume(length, breadth, thickness, width):
     return length * breadth * thickness * width
-
 
 # Update default values to floats
 default_values = {
@@ -25,13 +23,13 @@ if 'add_items' not in st.session_state:
 
 # Add a new empty add items row
 def add_item_row():
-    st.session_state.add_items.append(default_values)
+    st.session_state.add_items.append(default_values.copy())  # Use copy to avoid reference issues
 
 # Function to remove an item row by index with specific handling for index 0
 def remove_item_row(index):
     # If trying to delete index 0, and it's the only item, reset to default
     if index == 0 and len(st.session_state.add_items) == 1:
-        st.session_state.add_items[0] = default_values  # Reset to default values
+        st.session_state.add_items[0] = default_values.copy()  # Reset to default values
     elif index < len(st.session_state.add_items):
         del st.session_state.add_items[index]
 
@@ -39,7 +37,7 @@ total_sum = 0
 
 # Display current add items
 for i, items in enumerate(st.session_state.add_items):
-    item_name = st.text_input(f"Enter item {i+1}", key=f"item_name_{i}")
+    item_name = st.text_input(f"Enter item {i+1}", value=items['item_name'], key=f"item_name_{i}")
 
     col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 0.5, 0.15, 0.15])
     with col1:
