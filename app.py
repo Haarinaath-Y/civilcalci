f44de4c6c60654a401f075cdf463e74cc1791be9
Nameless Code <harishy.bio@gmail.com>
2024-10-30 14:02:26 +0530

    column addition

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def func():
 
 # Initialize session state for add items list if not already done
 if 'add_items' not in st.session_state:
-    st.session_state.add_items = [{"length":1,"breadth":1,"thickness":1,"width":1}]  # List to store add items
+    st.session_state.add_items = [{"length": 1, "breadth": 1, "thickness": 1, "width": 1}]  # List to store add items
 
 
 # Add a new empty add items row
@@ -29,7 +29,7 @@ def remove_item_row(index):
 item_name = st.text_input("Enter item name")
 # Display current add items
 for i, items in enumerate(st.session_state.add_items):
-    col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 0.5, 0.2, 0.2])
+    col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 0.5, 0.1, 0.1])
     with col1:
         length = st.number_input("Enter the length", value=1, key=f'length_{i}')
     with col2:
@@ -51,5 +51,3 @@ for i, items in enumerate(st.session_state.add_items):
         if st.button(":material/delete:", key=f"remove_{i}"):
             remove_item_row(i)
             st.rerun()  # Rerun to refresh the UI after deletion
-
-st.write(st.session_state.add_items)
