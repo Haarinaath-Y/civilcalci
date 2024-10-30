06e8f70d4b3a2de4188f97e604c3c83d6a0acf9f
Nameless Code <harishy.bio@gmail.com>
2024-10-30 13:33:01 +0530

    column addition

--- app.py
+++ app.py
@@ -31,13 +31,13 @@ item_name = st.text_input("Enter item name")
 for i, items in enumerate(st.session_state.add_items):
     col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 0.2, 0.2, 0.2])
     with col1:
-        length = st.number_input("Enter the length", value=1, key='length')
+        length = st.number_input("Enter the length", value=1, key=f'length_{i}')
     with col2:
-        breadth = st.number_input("Enter the breadth", value=1, key='breadth')
+        breadth = st.number_input("Enter the breadth", value=1, key=f'breadth_{i}')
     with col3:
-        thickness = st.number_input("Enter the thickness", value=1, key='thickness')
+        thickness = st.number_input("Enter the thickness", value=1, key=f'thickness_{i}')
     with col4:
-        width = st.number_input("Enter the width", value=1, key='width')
+        width = st.number_input("Enter the width", value=1, key=f'width_{i}')
     with col5:
         st.write('Area')
         st.text(func())
