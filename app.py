909a0123ae3d0e6f8799273c3ef46be073bf1a2a
Nameless Code <harishy.bio@gmail.com>
2024-10-30 13:26:07 +0530

    column addition

--- app.py
+++ app.py
@@ -10,25 +10,25 @@ def func():
     return a
 
 
-# Initialize session state for extra payments list if not already done
+# Initialize session state for add items list if not already done
 if 'add_items' not in st.session_state:
-    st.session_state.add_items = []  # List to store extra payments (month and amount)
+    st.session_state.add_items = []  # List to store add items
 
 
-# Add a new empty extra payment row
+# Add a new empty add items row
 def add_item_row():
     st.session_state.add_items.append({"month": None, "amount": 0})
 
 
-# Remove an extra payment row by index
+# Remove an add items row by index
 def remove_item_row(index):
     if index < len(st.session_state.add_items):
         del st.session_state.add_items[index]  # Delete entry at the specified index
 
 
-# Display current extra payments
+item_name = st.text_input("Enter item name")
+# Display current add items
 for i, items in enumerate(st.session_state.add_items):
-    item_name = st.text_input("Enter item name")
     col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 1, 1, 1, 0.2, 0.2, 0.2])
     with col1:
         length = st.number_input("Enter the length", value=1)
@@ -51,4 +51,10 @@ for i, items in enumerate(st.session_state.add_items):
         if st.button(":material/delete:", key=f"remove_{i}"):
             remove_item_row(i)
             st.rerun()  # Rerun to refresh the UI after deletion
+
+
+# Add new extra payment row button
+if st.button("👉 Click here to Add Extra Payments"):
+    add_item_row()
+    st.rerun()
 
