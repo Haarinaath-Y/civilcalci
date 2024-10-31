3927a03d92ae9af76a351047eddcd9423b4cd7e6
Nameless Code <harishy.bio@gmail.com>
2024-10-31 17:25:59 +0530

    round bar - multi function setup

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pandas import DataFrame
 
-st.set_page_config(page_title="Civil Material Calculator", page_icon=":material/calculate:", layout="wide")
+st.set_page_config(page_title="Civil Material Calculator", page_icon=":material/calculate:")
 st.sidebar.success("Navigate yourself")
 
 st.title("Civil Material Calculator")
@@ -93,7 +93,7 @@ def round_bar_func():
         return ln * dia
 
     total_sum = 0
-    col1, col2, col3, col4, col5 = st.columns([1, 1, 0.2, 0.1, 0.1])
+    col1, col2, col3, col4, col5 = st.columns([1, 1, 0.2, 0.05, 0.05])
 
     with col1:
         length = st.number_input("Enter the length (mm)", value=float(items['length']), min_value=0.0, key=f'length_{i}')
