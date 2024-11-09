7b8bc5648f298d952a65d297ba329c0646f92b45
Nameless Code <harishy.bio@gmail.com>
2024-11-09 10:29:17 +0530

    pdf report

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def create_pdf(dataframe, total_sum):
         # Move to the next row
         y_offset -= line_height
 
-    p.drawString(30, y_offset - 20, f"Total Weight: {total_sum}")
+    p.drawString(30, y_offset - 20, f"**Total Weight**: {total_sum} kg")
 
     p.save()
     buffer.seek(0)
